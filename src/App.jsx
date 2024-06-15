c663bc64716920253c1dbb56f4e97d722438cf57
nielvid <nielvidservices@gmail.com>
2024-06-15 14:33:06 +0100

    initial setup of project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./components/Hero";
+import TopHeader from "./components/TopHeader";
+
+function App() {
+ const name = 'paul'
+ console.log(name)
+ return (
+   <div >
+    <TopHeader />
+    <Hero />
+   </div>
+ )
+}
+
+
+export default App;
