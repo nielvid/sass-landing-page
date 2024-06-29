b5c00b7a0668ec10f004fe7d4c7268614b24c5be
nielvid <nielvidservices@gmail.com>
2024-06-29 14:45:12 +0100

    hero section 95 percent complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,44 @@
-
-
+import Image237 from '../assets/images/icons/Orion_arrow-up-circle.png'
+import Conversion from '../assets/images/icons/conversion.png'
+import BgImage from '../assets/images/shutterstock_453643495-removebg.png'
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      <div className='hero'>
+        <div className='hero__content'>
+          <p className='agency'>
+            1 Agency for Designing awesome website on planet earth
+          </p>
+          <div className='hero__content-main'>
+            <h2 className='hero__header'>
+              We design complete for
+              <br /> Online Saas Business
+            </h2>
+            <div className='hero__img__wrapper'>
+              <img
+                src={Image237}
+                alt='237'
+                width={80}
+                className='hero__img-1'
+              />
+            </div>
+          </div>
+          <div className='hero__cta'>
+            <button className='hero__btn'>Get free Design Consultation</button>
+            <p>
+              Get a Creative, Modern and Custom-Made <br /> for you Business
+              today
+            </p>
+          </div>
+        </div>
+        <div className='hero__wrapper'>
+          <img src={Conversion} alt='237' width={80} className='hero__img-2' />
+        </div>
+
+        <div className='bg-image-wrapper'>
+          <img src={BgImage} alt='237' className='bg-img' />
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -2,22 +2,23 @@ import Logo from '../assets/logo.png'
 
 export default function TopHeader() {
   return (
-    <div className="top-header">
-      <div className="top-header__logo">
-        <img src={Logo} alt="logo" />
-      </div>
-      <div className="top-header__nav">
-       
+    <div>
+      <div className='top-header'>
+        <div className='top-header__logo'>
+          <img src={Logo} alt='logo' />
+        </div>
+        <div className='top-header__nav'>
           <li>About</li>
           <li>Portfolio</li>
           <li>Blog</li>
           <li>Services</li>
           <li>Contact</li>
-        
+        </div>
+        <div className='top-header__cta'>
+          <button>Get free Call</button>
+        </div>
       </div>
-      <div className="top-header__cta">
-        <button>Get free Call</button>
-      </div>
+      <hr className='top-header__hr' />
     </div>
   )
 }
